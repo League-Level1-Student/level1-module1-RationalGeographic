c73ea94189b595ace6ccac825fef0c671e47ddd2
League <league@WTS9.attlocal.net>
2018-03-30 18:30:09 -0700

    lolzers
    construction stuff

---
+++ src/elephant.java
@@ -0,0 +1,21 @@
+
+public class elephant {
+	int leavesEaten = 9;
+	String home = "africa";
+
+	public static void yolo(int leavesEaten) {
+		System.out.println(leavesEaten);
+	}
+
+	public static void olo(String home) {
+		System.out.println(home);
+	}
+
+	elephant(int macbook, String lol) {
+this.leavesEaten = macbook; 
+	}
+	elephant african = new elephant(10, africa);
+	
+	
+	
+}
