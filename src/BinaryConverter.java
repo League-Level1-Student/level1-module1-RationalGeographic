eb84cb50a3ea12655fa5f86394dc380f21d22ba6
RationalGeographic <league@WTS20.attlocal.net>
2018-09-14 18:27:59 -0700

    makingnewint
    to convetertien

--- src/BinaryConverter.java
+++ src/BinaryConverter.java
@@ -10,9 +10,7 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-
-
-public class BinaryConverter  implements ActionListener  {
+public class BinaryConverter implements ActionListener {
 
 	String convert(String input) {
 		if (input.length() != 8) {
@@ -25,9 +23,26 @@ public class BinaryConverter  implements ActionListener  {
 			return "-";
 		}
 		try {
-			int asciiValue = Integer.parseInt(input, 2);
-			char theLetter = (char) asciiValue;
-			return "" + theLetter;
+			
+			int eight = input.charAt(1);
+			int seven = input.charAt(2);
+			int six = input.charAt(3);
+			int five = input.charAt(4);
+			int four = input.charAt(5);
+			int three = input.charAt(6);
+			int two = input.charAt(7);
+			int one = input.charAt(8);
+			
+			if(one==1) {
+				int outputone = 1;
+			}
+			else if ( one==0) {
+				 int outputone = 0;
+			}
+			if()
+			
+			//    https://www.thecrazyprogrammer.com/wp-content/uploads/2015/11/binary-to-decimal-example.png
+		
 		} catch (Exception e) {
 			JOptionPane.showMessageDialog(null, "Enter a binary, silly!!!");
 			return "-";
@@ -36,42 +51,31 @@ public class BinaryConverter  implements ActionListener  {
 
 	public static void main(String[] args) {
 
-		
-		
-		
 		BinaryConverter biguy = new BinaryConverter();
-		
+
 		biguy.newui();
-		
-		
-		
-		
+
 	}
-	
-	
+
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JLabel label2 = new JLabel();
 	JTextField answer = new JTextField(20);
 	JButton button = new JButton("convert");
-	
-	
+
 	private void newui() {
-	
+
 		frame.setTitle("binary boi");
-		
+
 		frame.add(panel);
 		frame.show();
 		answer.show();
 
-		
 		button.addActionListener(this);
-		
-		
+
 		button.show();
 		panel.add(answer);
 		panel.add(button);
-		
 
 		label2.show();
 		label2.setText("a");
@@ -80,12 +84,13 @@ public class BinaryConverter  implements ActionListener  {
 		frame.pack();
 		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
 	}
-	@Override 
-	public void actionPerformed(ActionEvent e) {
-		
-String text = answer.getText();
 
-System.out.println(convert(text));		
-		}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		String text = answer.getText();
+
+		System.out.println(convert(text));
+	}
 
 }
