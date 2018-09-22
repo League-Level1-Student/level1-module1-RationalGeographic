cb3abf64abda6393f529c93c1ddcbeb83be2f07c
RationalGeographic <league@WTS20.attlocal.net>
2018-09-21 18:28:25 -0700

    binerdy
    compeer

--- src/BinaryConverter.java
+++ src/BinaryConverter.java
@@ -22,29 +22,79 @@ public class BinaryConverter implements ActionListener {
 			JOptionPane.showMessageDialog(null, "Binary can only contain 1s or 0s, silly!!!");
 			return "-";
 		}
+		int outputone = 0;
+		int outputtwo = 0;
+		int outputthree = 0;
+		int outputfour = 0;
+		int outputfive = 0;
+		int outputsix =  0;
+		int outputseven = 0;
+		int outputeight = 0;
+		
 		try {
 			
-			int eight = input.charAt(1);
-			int seven = input.charAt(2);
-			int six = input.charAt(3);
+			int eight = input.charAt(7);
+			int seven = input.charAt(6);
+			int six = input.charAt(5);
 			int five = input.charAt(4);
-			int four = input.charAt(5);
-			int three = input.charAt(6);
-			int two = input.charAt(7);
-			int one = input.charAt(8);
+			int four = input.charAt(3);
+			int three = input.charAt(2);
+			int two = input.charAt(1);
+			int one = input.charAt(0);
 			
 			if(one==1) {
-				int outputone = 1;
+				 outputone = 1;
 			}
 			else if ( one==0) {
-				 int outputone = 0;
+				  outputone = 0;
 			}
 			if(two==1) {
-				int outputone = 1;
+				 outputtwo = 1;
 			}
 			else if ( two==0) {
-				 int outputone = 0;
+				  outputtwo = 0;
 			}
+			if(three==1) {
+				 outputthree = 1;
+			}
+			else if ( three==0) {
+				  outputthree = 0;
+			}
+			if(four==1) {
+				 outputfour = 1;
+			}
+			else if ( four==0) {
+				  outputfour = 0;
+			}if(five==1) {
+				 outputfive = 1;
+			}
+			else if ( five==0) {
+				 outputfive = 0;
+			}
+			if(six==1) {
+				 outputsix = 1;
+			}
+			else if ( six==0) {
+				  outputsix = 0;
+			}if(seven==1) {
+				 outputseven = 1;
+			}
+			else if ( seven==0) {
+				  outputseven = 0;
+			}
+			if(eight==1) {
+				 outputeight = 1;
+			}
+			else if ( eight==0) {
+				  outputeight = 0;
+			}
+			if(outputone==1) {
+				int newcon = 1+(2*0);
+			}
+			if(outputtwo==1) {
+				int newcon2 = 1*(2*1)
+			}
+			
 			
 			//    https://www.thecrazyprogrammer.com/wp-content/uploads/2015/11/binary-to-decimal-example.png
 		
@@ -53,6 +103,7 @@ public class BinaryConverter implements ActionListener {
 			return "-";
 		}
 	}
+
 
 	public static void main(String[] args) {
 
