3225c6b66c49b8cdac5abbca5175d4f69fea9576
blackbeltcam <league@iMac.attlocal.net>
2018-09-28 18:12:30 -0700

    binaryNUE
    from80+24

--- src/BinaryConverter.java
+++ src/BinaryConverter.java
@@ -10,98 +10,37 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+
 public class BinaryConverter implements ActionListener {
 
-	String convert(String input) {
+	void convert(String input) {
 		if (input.length() != 8) {
 			JOptionPane.showMessageDialog(null, "Enter 8 bits, silly!!!");
-			return "-";
+	
 		}
 		String binary = "[0-1]+"; // must contain numbers in the given range
 		if (!input.matches(binary)) {
 			JOptionPane.showMessageDialog(null, "Binary can only contain 1s or 0s, silly!!!");
-			return "-";
-		}
-		int outputone = 0;
-		int outputtwo = 0;
-		int outputthree = 0;
-		int outputfour = 0;
-		int outputfive = 0;
-		int outputsix =  0;
-		int outputseven = 0;
-		int outputeight = 0;
 		
-		try {
+		}
+		try{
+			int sum = 0;
+			for (int i = 0; i < input.length(); i++) {
+			if(input.charAt(i)==('1')) {
+				sum+=Math.pow(2, 7-i);
+				
+			}
+				
+			}
 			
-			int eight = input.charAt(7);
-			int seven = input.charAt(6);
-			int six = input.charAt(5);
-			int five = input.charAt(4);
-			int four = input.charAt(3);
-			int three = input.charAt(2);
-			int two = input.charAt(1);
-			int one = input.charAt(0);
+		System.out.println(sum);
 			
-			if(one==1) {
-				 outputone = 1;
-			}
-			else if ( one==0) {
-				  outputone = 0;
-			}
-			if(two==1) {
-				 outputtwo = 1;
-			}
-			else if ( two==0) {
-				  outputtwo = 0;
-			}
-			if(three==1) {
-				 outputthree = 1;
-			}
-			else if ( three==0) {
-				  outputthree = 0;
-			}
-			if(four==1) {
-				 outputfour = 1;
-			}
-			else if ( four==0) {
-				  outputfour = 0;
-			}if(five==1) {
-				 outputfive = 1;
-			}
-			else if ( five==0) {
-				 outputfive = 0;
-			}
-			if(six==1) {
-				 outputsix = 1;
-			}
-			else if ( six==0) {
-				  outputsix = 0;
-			}if(seven==1) {
-				 outputseven = 1;
-			}
-			else if ( seven==0) {
-				  outputseven = 0;
-			}
-			if(eight==1) {
-				 outputeight = 1;
-			}
-			else if ( eight==0) {
-				  outputeight = 0;
-			}
-			if(outputone==1) {
-				int newcon = 1+(2*0);
-			}
-			if(outputtwo==1) {
-				int newcon2 = 1*(2*1)
-			}
-			if(outputthree==1) {
-				int newcon3 = 
 			
 			//    https://www.thecrazyprogrammer.com/wp-content/uploads/2015/11/binary-to-decimal-example.png
 		
 		} catch (Exception e) {
 			JOptionPane.showMessageDialog(null, "Enter a binary, silly!!!");
-			return "-";
+			
 		}
 	}
 
@@ -147,7 +86,7 @@ public class BinaryConverter implements ActionListener {
 
 		String text = answer.getText();
 
-		System.out.println(convert(text));
+		convert(text);
 	}
 
 }
